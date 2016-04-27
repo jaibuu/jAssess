80afa340827d48ead051c062a80ef599272a7add
Jairo Narvaez <jaibuu+git@gmail.com>
2016-04-27 10:25:29 -0500

    Added router

--- client/main.js
+++ client/main.js
@@ -1,15 +1,20 @@
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+  // // counter starts at 0
+  // Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
+  // Template.hello.helpers({
+  //   counter: function () {
+  //     return Session.get('counter');
+  //   }
+  // });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 10);
-    }
-  });
+  // Template.hello.events({
+  //   'click button': function () {
+  //     // increment the counter when button is clicked
+  //     Session.set('counter', Session.get('counter') + 10);
+  //   }
+  // });
+
+
+Router.route('/', function () {
+  this.render('home');
+});
